073ac79d7c7bdaf1421511b53fdb013ff239faef
DucHaiten <121789980+DucHaiten@users.noreply.github.com>
2026-08-01 11:20:10 +0700

    Update LuuX offline cache

--- public/luux/sw.js
+++ public/luux/sw.js
@@ -1,35 +1,35 @@
-const VERSION = 'luux-loader-v1';
-const APP = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
-const PACKAGE = [0, 1, 2, 3].map((n) => `https://raw.githubusercontent.com/DucHaiten/pixel-foundry-web/main/.luux/part-0${n}`);
+const CACHE = 'luux-v2';
+const APP = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(VERSION).then(async (cache) => {
-    await cache.addAll(APP);
-    await Promise.all(PACKAGE.map(async (url) => {
-      try {
-        const response = await fetch(url, { cache: 'no-store' });
-        if (response.ok) await cache.put(url, response);
-      } catch {}
-    }));
-  }));
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(APP)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== VERSION).map((key) => caches.delete(key)))));
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))));
   self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+
   if (event.request.mode === 'navigate') {
-    event.respondWith(fetch(event.request).catch(() => caches.match('./index.html')));
+    event.respondWith(fetch(event.request).then((response) => {
+      const copy = response.clone();
+      caches.open(CACHE).then((cache) => cache.put('./index.html', copy));
+      return response;
+    }).catch(() => caches.match('./index.html')));
     return;
   }
+
+  if (url.origin !== self.location.origin) return;
+
   event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
-    if (response.ok || response.type === 'opaque') {
+    if (response.ok) {
       const copy = response.clone();
-      caches.open(VERSION).then((cache) => cache.put(event.request, copy));
+      caches.open(CACHE).then((cache) => cache.put(event.request, copy));
     }
     return response;
   })));
